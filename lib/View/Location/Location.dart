2f0354e0e8b056ea2981b12fd196d48d1e9d70e2
Nazakat Hussain <nazakat786h@gmail.com>
2023-09-28 16:18:10 +0500

    fix some bug

--- lib/View/Location/Location.dart
+++ lib/View/Location/Location.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
+
 class Location extends StatefulWidget {
   const Location({super.key});
 
@@ -39,7 +40,9 @@ class _LocationState extends State<Location> {
       ),
       body: Column(
         children: [
-          CustomSizeBox(height: 30.h,),
+          CustomSizeBox(
+            height: 30.h,
+          ),
           Expanded(
             child: Container(
               width: double.infinity,
@@ -53,94 +56,106 @@ class _LocationState extends State<Location> {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  CustomSizeBox(height: 30.h,),
-                  Image.asset('assets/images/Location/current.png',height: 290.h,width: 290.w,),
-                  CustomSizeBox(height: 30.h,),
+                  CustomSizeBox(
+                    height: 30.h,
+                  ),
+                  Image.asset(
+                    'assets/images/Location/current.png',
+                    height: 290.h,
+                    width: 290.w,
+                  ),
+                  CustomSizeBox(
+                    height: 30.h,
+                  ),
                   Expanded(
-                    child: Container(
-                      width: double.infinity,
-                      child: Card(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.only(
-                            topRight: Radius.circular(35.r),
-                            topLeft: Radius.circular(35.r),
-                          ),
-                        ),
-                        elevation: 3,
-                        child: Padding(
-                          padding:  EdgeInsets.only(top: 20.h,left: 20.w,right: 20.w),
-                          child: Column(
-                            children: [
-                              Text(
-                                'Enable Location Services',
-                                style: TextStyle(
-                                    fontFamily: "Nuntio",
-                                    fontSize: 24.sp,
-                                    fontWeight: FontWeight.w700,
-                                    color: Colors.black),
-                              ),
-                              CustomSizeBox(height: 20.h,),
-                              Text(
-                                'Turn on location services so we can show you whats nearby',
-                                style: TextStyle(
-                                    fontFamily: "Nuntio",
-                                    fontSize: 16.sp,
-                                    fontWeight: FontWeight.w400,
-                                    color: Colors.black),
-                                textAlign: TextAlign.center,
-                              ),
-                              CustomSizeBox(height: 60.h,),
-                              ElevatedButton(
-                                onPressed: () {
-
-                                },
-                                child: Text(
-                                  'Allow Location Access',
-                                  style: TextStyle(
-                                    fontFamily: "Nuntio",
-                                    fontSize: 16.sp,
-                                    fontWeight: FontWeight.w800,
-                                    color: Colors.white,
-                                  ),
-                                ),
-                                style: ElevatedButton.styleFrom(
-                                    fixedSize: Size(300.w, 50.h),
-                                    backgroundColor: Color(0xff6F45F0),
-                                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(32.r))
-                                ),
-                              ),
-                              CustomSizeBox(height: 20.h,),
-                              ElevatedButton(
-                                onPressed: () {
-                                    Get.to(MapView());
-                                },
-                                child: Text(
-                                  'Enter my Location',
-                                  style: TextStyle(
-                                    fontFamily: "Nuntio",
-                                    fontSize: 16.sp,
-                                    fontWeight: FontWeight.w800,
-                                    color: Colors.grey
-                                  ),
-                                ),
-                                style: ElevatedButton.styleFrom(
-                                    fixedSize: Size(300.w, 50.h),
-                                    backgroundColor: Colors.white,
-                                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(32.r),
-                                    side: BorderSide(
-                                      width: 2.w,
-                                      color: Colors.grey
-                                    )
-                                    ),
-                                ),
-                              ),
-                            ],
-                          ),
+                      child: Container(
+                    width: double.infinity,
+                    child: Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(35.r),
+                          topLeft: Radius.circular(35.r),
                         ),
                       ),
-                  )
-                  ),
-                    ],
+                      elevation: 3,
+                      child: Padding(
+                        padding:
+                            EdgeInsets.only(top: 20.h, left: 20.w, right: 20.w),
+                        child: Column(
+                          children: [
+                            Text(
+                              'Enable Location Services',
+                              style: TextStyle(
+                                  fontFamily: "Nuntio",
+                                  fontSize: 24.sp,
+                                  fontWeight: FontWeight.w700,
+                                  color: Colors.black),
+                            ),
+                            CustomSizeBox(
+                              height: 20.h,
+                            ),
+                            Text(
+                              'Turn on location services so we can show you whats nearby',
+                              style: TextStyle(
+                                  fontFamily: "Nuntio",
+                                  fontSize: 16.sp,
+                                  fontWeight: FontWeight.w400,
+                                  color: Colors.black),
+                              textAlign: TextAlign.center,
+                            ),
+                            CustomSizeBox(
+                              height: 60.h,
+                            ),
+                            ElevatedButton(
+                              onPressed: () {},
+                              child: Text(
+                                'Allow Location Access',
+                                style: TextStyle(
+                                  fontFamily: "Nuntio",
+                                  fontSize: 16.sp,
+                                  fontWeight: FontWeight.w800,
+                                  color: Colors.white,
+                                ),
+                              ),
+                              style: ElevatedButton.styleFrom(
+                                  fixedSize: Size(300.w, 50.h),
+                                  backgroundColor: Color(0xff6F45F0),
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius:
+                                          BorderRadius.circular(32.r))),
+                            ),
+                            CustomSizeBox(
+                              height: 20.h,
+                            ),
+                            ElevatedButton(
+                              onPressed: () {
+                                Get.to(MapView());
+                              },
+
+
+                              child: Text(
+                                'Enter my Location',
+                                style: TextStyle(
+                                    fontFamily: "Nuntio",
+                                    fontSize: 16.sp,
+                                    fontWeight: FontWeight.w800,
+                                    color: Colors.grey),
+                              ),
+                              style: ElevatedButton.styleFrom(
+                                fixedSize: Size(300.w, 50.h),
+                                backgroundColor: Colors.white,
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(32.r),
+                                    side: BorderSide(
+                                        width: 2.w, color: Colors.grey)),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  )),
+                ],
               ),
             ),
           ),
--- lib/View/bottomNavBar.dart
+++ lib/View/bottomNavBar.dart
@@ -1,4 +1,3 @@
-
 import 'package:barbar_saloon/View/Booking/Booking.dart';
 import 'package:barbar_saloon/View/Home/Home.dart';
 import 'package:barbar_saloon/View/Location/Location.dart';
@@ -7,6 +6,7 @@ import 'package:barbar_saloon/View/Profile/Profilr.dart';
 import 'package:flutter/material.dart';
 
 import 'App Booking/Booki.dart';
+
 class BottomNavBar extends StatefulWidget {
   const BottomNavBar({super.key});
 
@@ -16,30 +16,57 @@ class BottomNavBar extends StatefulWidget {
 
 class _BottomNavBarState extends State<BottomNavBar> {
   int currentIndex = 0;
-  List tabs = [Home(),Location(),Booki(),Message(),Profile()];
+  List tabs = [Home(), Location(), Booki(), Message(), Profile()];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: tabs[currentIndex],
-      bottomNavigationBar:BottomNavigationBar(
+      bottomNavigationBar: BottomNavigationBar(
           elevation: 2,
-          onTap: (index){
+          onTap: (index) {
             setState(() {
               currentIndex = index;
             });
           },
-          selectedItemColor: Colors.black, // Change the selected label text color
+          selectedItemColor:
+              Colors.black, // Change the selected label text color
           unselectedItemColor: Colors.grey,
-          items:[
-            BottomNavigationBarItem(icon: Icon(Icons.home,color: Colors.grey,),label: 'Home'),
-            BottomNavigationBarItem(icon: Icon(Icons.location_on,color: Colors.grey,),label: 'Location'),
-            BottomNavigationBarItem(icon: Icon(Icons.file_present,color: Colors.grey,),label: 'Booking'),
-            BottomNavigationBarItem(icon: Icon(Icons.message,color: Colors.grey,),label: 'Message'),
-            BottomNavigationBarItem(icon: Icon(Icons.person,color: Colors.grey,),label: 'Profile',),
-          ]
-
-      ),
+          selectedLabelStyle: TextStyle(
+            color: Colors.black,
+          ),
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.home,
+                  color: Colors.grey,
+                ),
+                label: 'Home'),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.location_on,
+                  color: Colors.grey,
+                ),
+                label: 'Location'),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.file_present,
+                  color: Colors.grey,
+                ),
+                label: 'Booking'),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.message,
+                  color: Colors.grey,
+                ),
+                label: 'Message'),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.person,
+                color: Colors.grey,
+              ),
+              label: 'Profile',
+            ),
+          ]),
     );
   }
 }
-
